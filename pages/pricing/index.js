22c662e50f6e69bd955fef9cfb0a9b825b7b0544
Kevin Seabourne <kevinseabourne@hotmail.com>
2020-08-11 23:33:22 +0800

    style change

--- pages/pricing/index.js
+++ pages/pricing/index.js
@@ -5,30 +5,33 @@ import ImageLoader from "../../components/common/imageLoader";
 const Pricing = (props) => {
   return (
     <Container>
-      <Link href="/pricing/[id]" as="/pricing/weddings">
-        <WeddingsContainer>
-          <ImageLoader
-            maxWidth="700px"
-            placeholderSize="66.7%"
-            borderRadius="8px"
-            boxShadow="0px 20px 40px rgba(0,0,0,0.4)"
-            src="https://chpistel.sirv.com/Images/kal-visuals-lYn248p4rUg-unsplash.jpg?"
-          />
-          <Title>Weddings</Title>
-        </WeddingsContainer>
-      </Link>
-      <Link href="/pricing/[id]" as="pricing/corporate">
-        <CorporateContainer>
-          <ImageLoader
-            maxWidth="700px"
-            placeholderSize="66.7%"
-            borderRadius="8px"
-            boxShadow="0px 20px 40px rgba(0,0,0,0.4)"
-            src="https://chpistel.sirv.com/Images/kal-visuals-lYn248p4rUg-unsplash.jpg?"
-          />
-          <Title>Corporate</Title>
-        </CorporateContainer>
-      </Link>
+      <Title>Pricing</Title>
+      <InnerContainer>
+        <Link href="/pricing/[id]" as="/pricing/weddings">
+          <WeddingsContainer>
+            <ImageLoader
+              maxWidth="700px"
+              placeholderSize="66.7%"
+              borderRadius="8px"
+              boxShadow="0px 20px 40px rgba(0,0,0,0.4)"
+              src="https://chpistel.sirv.com/Images/kal-visuals-lYn248p4rUg-unsplash.jpg?"
+            />
+            <Label>Weddings</Label>
+          </WeddingsContainer>
+        </Link>
+        <Link href="/pricing/[id]" as="pricing/corporate">
+          <CorporateContainer>
+            <ImageLoader
+              maxWidth="700px"
+              placeholderSize="66.7%"
+              borderRadius="8px"
+              boxShadow="0px 20px 40px rgba(0,0,0,0.4)"
+              src="https://chpistel.sirv.com/Images/kal-visuals-lYn248p4rUg-unsplash.jpg?"
+            />
+            <Label>Corporate</Label>
+          </CorporateContainer>
+        </Link>
+      </InnerContainer>
     </Container>
   );
 };
@@ -37,8 +40,19 @@ export default Pricing;
 
 const Container = styled.div`
   width: 100%;
-  padding-top: 40px;
   display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  margin: 40px 0px;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  width: 100%;
   align-items: center;
   justify-content: space-around;
   flex-direction: row;
@@ -48,15 +62,25 @@ const WeddingsContainer = styled.div`
   max-width: 700px;
   width: 100%;
   position: relative;
+  transition: all 0.25s ease;
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.03);
+  }
 `;
 
 const CorporateContainer = styled.div`
   max-width: 700px;
   width: 100%;
   position: relative;
+  transition: all 0.25s ease;
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.03);
+  }
 `;
 
-const Title = styled.label`
+const Label = styled.label`
   font-size: 2rem;
   position: absolute;
   bottom: 20px;
