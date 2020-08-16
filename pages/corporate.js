6bc6536408c83c26cfb727d64b5698864b40ebd1
kevinseabourne <kevinseabourne@hotmail.com>
2020-08-17 02:27:02 +0800

    started corporate page

--- pages/corporate.js
+++ pages/corporate.js
@@ -1,9 +1,47 @@
 import styled from "styled-components";
+import ImageLoader from "../components/common/imageLoader";
 
 const Corporate = (props) => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Title>Corporate Videos</Title>
+      <Description></Description>
+      <InnerContainer>
+        <Item>
+          <ImageContainer>
+            <ImageLoader
+              maxWidth="430px"
+              placeholderSize="66.7%"
+              hover={true}
+              src="https://chpistel.sirv.com/Connor-Portfolio/wedding_photo.png?png.optimize=true"
+            />
+          </ImageContainer>
+          <CompanyTitle>TLG</CompanyTitle>
+        </Item>
+      </InnerContainer>
+    </Container>
+  );
 };
 
 export default Corporate;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1``;
+
+const Description = styled.p``;
+
+const InnerContainer = styled.div`
+  display: grid;
+  width: 100%;
+`;
+
+const Item = styled.div``;
+
+const ImageContainer = styled.div``;
+
+const CompanyTitle = styled.label``;
