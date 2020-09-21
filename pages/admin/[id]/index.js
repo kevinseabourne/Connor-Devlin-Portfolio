b8047dda1c04869afb99e443a6ed0c2ae02084ae
kevinseabourne <kevinseabourne@hotmail.com>
2020-09-21 18:15:33 +0800

    added new routes

--- pages/admin/[id]/index.js
+++ pages/admin/[id]/index.js
@@ -1,34 +1,67 @@
 import styled from "styled-components";
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import AdminAbout from "../../../components/adminAbout";
 import AdminContentPage from "../../../components/common/adminContentPage";
+import AdminPricing from "../../../components/adminPricing";
+import AdminAddContent from "../../../components/adminAddContent";
+import AdminEditContent from "../../../components/adminEditContent";
+import AdminDeleteContent from "../../../components/adminDeleteContent";
 import { getAllWeddings } from "../../api/weddings";
 import { getAllCorporate } from "../../api/corporate";
-// import WeddingPricing from "../../../components/weddingPricing";
-// import CorporatePricing from "../../../components/corporatePricing";
+import AdminSidebar from "../../../components/AdminSidebar";
 
 const AdminRoute = ({ params, data }) => {
-  const router = useRouter();
-  const [routes] = useState(["about", "weddings", "corporate"]);
-
-  if (params) {
-    const aboutRoute = params.id === "about";
-    const weddingsRoute = params.id === "weddings";
-    const corporateRoute = params.id === "corporate";
-
-    if (aboutRoute) {
-      return <AdminAbout />;
-    } else if (weddingsRoute || corporateRoute) {
+  switch (params.id) {
+    case "about":
       return (
-        <AdminContentPage
-          data={data}
-          page={weddingsRoute ? "weddings" : "corporate"}
-        />
+        <React.Fragment>
+          <AdminSidebar />
+          <AdminAbout />
+        </React.Fragment>
       );
-    }
-  } else {
-    return <Container />;
+    case "weddings":
+      return (
+        <React.Fragment>
+          <AdminSidebar />
+          <AdminContentPage data={data} page="weddings" />;
+        </React.Fragment>
+      );
+    case "corporate":
+      return (
+        <React.Fragment>
+          <AdminSidebar />
+          <AdminContentPage data={data} page="corporate" />;
+        </React.Fragment>
+      );
+    case "pricing":
+      return (
+        <React.Fragment>
+          <AdminSidebar />
+          <AdminPricing />;
+        </React.Fragment>
+      );
+    case "add-content":
+      return (
+        <React.Fragment>
+          <AdminSidebar />
+          <AdminAddContent />;
+        </React.Fragment>
+      );
+    case "edit-content":
+      return (
+        <React.Fragment>
+          <AdminSidebar />
+          <AdminEditContent />;
+        </React.Fragment>
+      );
+    case "delete-content":
+      return (
+        <React.Fragment>
+          <AdminSidebar />
+          <AdminDeleteContent />;
+        </React.Fragment>
+      );
+    default:
+      return <Container />;
   }
 };
 
@@ -38,17 +71,29 @@ export async function getStaticPaths() {
       { params: { id: "about" } },
       { params: { id: "weddings" } },
       { params: { id: "corporate" } },
+      { params: { id: "pricing" } },
+      { params: { id: "add-content" } },
+      { params: { id: "edit-content" } },
+      { params: { id: "delete-content" } },
     ],
     fallback: false,
   };
 }
 
 export async function getStaticProps({ params }) {
-  const data =
-    params.id === "weddings" ? await getAllWeddings() : await getAllCorporate();
-  return {
-    props: { data, params },
-  };
+  if (params.id === "weddings" || params.id === "corporate") {
+    const data =
+      params.id === "weddings"
+        ? await getAllWeddings()
+        : await getAllCorporate();
+    return {
+      props: { data, params },
+    };
+  } else {
+    return {
+      props: { params },
+    };
+  }
 }
 
 export default AdminRoute;
