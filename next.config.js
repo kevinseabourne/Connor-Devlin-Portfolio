e6c351affda762264a26b9ac205e753305ec12dd
kevinseabourne <kevinseabourne@hotmail.com>
2020-10-30 21:03:21 +0800

    added file loader to web pack

--- next.config.js
+++ next.config.js
@@ -1,17 +1,30 @@
 module.exports = {
   // add more font or image formats if importing them to here.
   webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|woff|woff2|otf|ttf|svg)$/i,
-      use: [
-        {
-          loader: "url-loader",
-          options: {
-            limit: 100000,
+    config.module.rules.push(
+      {
+        test: /\.(png|jpg|gif|woff|woff2|otf|ttf|svg)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 100000,
+            },
           },
-        },
-      ],
-    });
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|mp3|aif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        ],
+      }
+    );
 
     return config;
   },
