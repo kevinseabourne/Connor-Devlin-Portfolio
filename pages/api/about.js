a08cdc18690cb116f09d379717599e7c6690f513
kevinseabourne <kevinseabourne@hotmail.com>
2020-11-25 18:09:49 +0800

    added about page request functions

---
+++ pages/api/about.js
@@ -0,0 +1,64 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import moment from "moment";
+import { toast } from "react-toastify";
+
+const config_ = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_ENDPOINT,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config_);
+}
+
+export async function getAboutMe() {
+  const response = await firebase
+    .auth()
+    .signInWithEmailAndPassword(
+      process.env.NEXT_PUBLIC_FIREBASE_ADMIN_EMAIL,
+      process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PASSWORD
+    )
+    .then(async function () {
+      const db = firebase.firestore();
+      const aboutMe = await db
+        .collection("about")
+        .doc("aboutMe")
+        .get()
+        .then(function (doc) {
+          if (doc.exists) {
+            return doc.data();
+          }
+        })
+        .catch(function (error) {
+          console.log("Error getting document:", error);
+        });
+      return aboutMe;
+    });
+  return response;
+}
+
+export async function updateAboutMe(updatedAboutMe) {
+  const response = await firebase
+    .auth()
+    .signInWithEmailAndPassword(
+      process.env.NEXT_PUBLIC_FIREBASE_ADMIN_EMAIL,
+      process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PASSWORD
+    )
+    .then(async function () {
+      const db = firebase.firestore();
+      const aboutMe = await db
+        .collection("about")
+        .doc("aboutMe")
+        .update({ description: updatedAboutMe })
+        .catch(function (error) {
+          console.log("Error getting document:", error);
+        });
+      return aboutMe;
+    });
+  return response;
+}
