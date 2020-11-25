4788ee8e88b4db5ac87d733abf31408e383199d3
kevinseabourne <kevinseabourne@hotmail.com>
2020-11-25 18:17:27 +0800

    created admin edit pricing page. For editing a wedding pricing package

---
+++ components/adminEditPricing.js
@@ -0,0 +1,319 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import {
+  getAllPricingPackages,
+  updatePricingPackages,
+} from ".././pages/api/pricing";
+import { toast } from "react-toastify";
+import cloneDeep from "lodash/cloneDeep";
+import { useForm } from "react-hook-form";
+import { Input } from "./common/input";
+import ImageLoader from "./common/imageLoader";
+import { TextArea } from "./common/textArea";
+import { motion, AnimateSharedLayout } from "framer-motion";
+import { LoadingSpinner } from "./loading-spinner";
+
+const AdminEditPricing = ({ data }) => {
+  const [state, setState] = useState();
+  const [status, setStatus] = useState("idle");
+  const timeout = useRef(null);
+  const [packageDetailsLength, setPackageDetailsLength] = useState([
+    { title: "packageDetail", id: Math.floor(1000 + Math.random() * 9000) },
+  ]);
+
+  useEffect(() => {
+    // if (data) {
+    //   console.log(data);
+    // }
+    // !data &&
+    //   toast.error("An error has occurred", {
+    //     position: "bottom-right",
+    //     autoClose: 5000,
+    //     hideProgressBar: false,
+    //     closeOnClick: true,
+    //     pauseOnHover: true,
+    //     draggable: true,
+    //   });
+    const fetchData = async () => {
+      const response = await getAllPricingPackages();
+      console.log(response);
+    };
+    fetchData();
+    return () => clearTimeout(timeout.current);
+  }, []);
+
+  const { register, handleSubmit, reset, errors, unregister } = useForm();
+
+  const schema = {
+    packageDetail: {
+      required: "A name is required !",
+      minLength: {
+        value: 1,
+        message: "Package Detail should be greater than 1 character !",
+      },
+      // pattern: {
+      //   value: /^[a-zA-Z '.-]*$/,
+      //   message: "Letters only !",
+      // },
+    },
+  };
+
+  const onSubmit = async (data) => {
+    setStatus("pending");
+    // await updatePricing(aboutMe);
+    console.log(data);
+    // const response = await getAllPricingPackages();
+    // const { description } = response;
+    // setState(description);
+    reset();
+    setStatus("resolved");
+    timeout.current = setTimeout(() => setStatus("idle"), 3000);
+  };
+
+  const deletePackageDetail = (index) => {
+    // playDown();
+    const packageDetailsLengthClone = cloneDeep(packageDetailsLength);
+    if (index > -1 && packageDetailsLength.length > 1) {
+      unregister(`partnerFirstName_${packageDetailsLengthClone[index].id}`);
+      packageDetailsLengthClone.splice(index, 1);
+      setPackageDetailsLength(packageDetailsLengthClone);
+    }
+  };
+
+  const addPackageDetail = (index) => {
+    // play();
+    const packageDetailsLengthClone = cloneDeep(packageDetailsLength);
+    if (index > -1 && packageDetailsLengthClone.length <= 10) {
+      packageDetailsLengthClone.splice(index + 1, 0, {
+        title: "packageDetail",
+        id: Math.floor(1000 + Math.random() * 9000),
+      });
+      setPackageDetailsLength(packageDetailsLengthClone);
+    }
+  };
+
+  return (
+    <AnimateSharedLayout>
+      <Container layout>
+        <Title>Preview</Title>
+        <About layout>{state}</About>
+        <Title layout>Edit Pricing Packages</Title>
+        <Form onSubmit={handleSubmit(onSubmit)} layout>
+          <Input
+            name="packageName"
+            label="Title"
+            ref={register}
+            error={errors.name}
+          />
+          <Input
+            name="price"
+            label="Price"
+            ref={register}
+            error={errors.price}
+          />
+          <TextArea
+            name="description"
+            label="Description"
+            ref={register}
+            error={errors.description}
+          />
+
+          {packageDetailsLength.map((packageDetail) => (
+            <PackageDetailsContainer>
+              <AddPackageDetail
+                tabIndex="0"
+                role="button"
+                onClick={() =>
+                  addPackageDetail(packageDetailsLength.indexOf(packageDetail))
+                }
+              >
+                <ImageLoader
+                  maxWidth="inherit"
+                  placeholderSize="100%"
+                  opacity="0"
+                  transitionTime="300ms ease"
+                  hover={true}
+                  src={
+                    "https://chpistel.sirv.com/Connor-Portfolio/plus.png?w=23"
+                  }
+                />
+              </AddPackageDetail>
+              <Input
+                name={`packageDetail_${packageDetail.id}`}
+                label="Package Detail"
+                ref={register(schema.packageDetail)}
+                error={errors[`packageDetail_${packageDetail.id}`]}
+              />
+              <DeletePackageDetail
+                tabIndex="0"
+                role="button"
+                onClick={() =>
+                  deletePackageDetail(
+                    packageDetailsLength.indexOf(packageDetail)
+                  )
+                }
+              >
+                <ImageLoader
+                  maxWidth="inherit"
+                  placeholderSize="100%"
+                  opacity="0"
+                  transitionTime="300ms ease"
+                  hover={true}
+                  src={
+                    "https://chpistel.sirv.com/Connor-Portfolio/minus%20(1).png?w=23"
+                  }
+                />
+              </DeletePackageDetail>
+            </PackageDetailsContainer>
+          ))}
+          <SubmitButton
+            layout
+            type="submit"
+            whileTap={{ scale: 0.9 }}
+            disabled={status === "pending" ? true : false}
+          >
+            {status === "pending" ? (
+              <LoadingSpinner size={"28px"} />
+            ) : status === "resolved" ? (
+              "Success"
+            ) : (
+              "Update"
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    </AnimateSharedLayout>
+  );
+};
+
+export default AdminEditPricing;
+
+const Container = styled(motion.div)`
+  min-height: calc(100vh - 75px);
+  width: calc(100% - 280px);
+  margin-left: auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-image: ${({ theme }) =>
+    `linear-gradient(to right,  ${theme.colors.gradient1} 10%, ${theme.colors.gradient2} 100% )`};
+  margin-left: auto;
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
+`;
+
+const About = styled(motion.p)`
+  max-width: 660px;
+  width: 100%;
+  white-space: pre-wrap;
+  overflow-wrap: break-word;
+`;
+
+const Title = styled(motion.h1)`
+  margin: 70px 0px;
+  text-align: center;
+  @media (max-width: 750px) {
+    margin: 10% 0px;
+    font-size: 1.4rem;
+  }
+`;
+
+const PackageDetailsContainer = styled(motion.div)`
+  width: 100%;
+  position: relative;
+`;
+
+const AddPackageDetail = styled.div`
+  width: 20px;
+  display: flex;
+  margin-right: 10px;
+  position: absolute;
+  left: -40px;
+  top: 35px;
+  padding: 0px;
+  border: none;
+  background: none;
+  height: 20px;
+  &:focus:not(:focus-visible) {
+    outline: none;
+  }
+  @media (max-width: 609px) {
+    order: 4;
+    bottom: 22px;
+    top: unset;
+    width: 23px;
+    left: 90px;
+  }
+  @media (max-width: 340px) {
+    left: 50px;
+  }
+`;
+
+const DeletePackageDetail = styled.div`
+  width: 20px;
+  margin-left: 10px;
+  position: absolute;
+  right: -40px;
+  top: 35px;
+  padding: 0px;
+  border: none;
+  background: none;
+  height: 20px;
+  &:focus:not(:focus-visible) {
+    outline: none;
+  }
+  @media (max-width: 609px) {
+    position: absolute;
+    bottom: 22px;
+    top: unset;
+    width: 23px;
+    right: 90px;
+  }
+  @media (max-width: 340px) {
+    right: 50px;
+  }
+`;
+
+const Form = styled(motion.form)`
+  width: 100%;
+  max-width: 650px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const SubmitButton = styled(motion.button)`
+  margin-top: 10px;
+  font-size: 1rem;
+  min-height: 54px;
+  min-width: 200px;
+  max-width: 214.23px;
+  padding: 18px 80px;
+  border-radius: 9px;
+  border: none;
+  color: white;
+  position: relative;
+  font-weight: 600;
+  box-shadow: rgba(0, 0, 0, 0.02) 0px -5.9px 2.7px,
+    rgba(0, 0, 0, 0.024) 0px -1.2px 6.9px, rgba(0, 0, 0, 0.03) 0px 8px 14.2px,
+    rgba(0, 0, 0, 0.04) 0px 21.9px 29.2px, rgba(0, 0, 0, 0.07) 0px 49px 80px;
+  transition: all 0.2s ease;
+  background-image: ${({ theme }) =>
+    `radial-gradient( circle farthest-corner at 10% 20%,  ${theme.colors.gradient1} 0%, ${theme.colors.gradient2} 100.2% )`};
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+  &:focus:not(:focus-visible) {
+    outline: none;
+  }
+  @media (max-width: 609px) {
+    margin-bottom: 60px;
+    max-width: 100%;
+    width: 100%;
+  }
+`;
