57bf47dc4854d6c7cac5eedc650e60f6b427b91e
kevinseabourne <kevinseabourne@hotmail.com>
2020-12-24 16:20:58 +0800

    favicon and social media preview meta tags added to header

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,56 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta property="og:title" content="Connor Devlin Media" />
+          <meta
+            property="og:description"
+            content="Digital media producer and filmmaker"
+          />
+          <meta
+            property="og:image"
+            content="https://chpistel.sirv.com/Connor-Portfolio/browser-Screenshot.png?w=700"
+          />
+          <meta
+            property="og:url"
+            content="https://connor-devlin-portfolio.vercel.app/"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
