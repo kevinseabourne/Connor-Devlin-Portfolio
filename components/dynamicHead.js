3459b142915f4908c608266995a4e42965c96320
kevinseabourne <kevinseabourne@hotmail.com>
2020-12-25 01:16:48 +0800

    created reusable Head component for dynamic meta tags

---
+++ components/dynamicHead.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const DynamicHead = ({ title, description, urlQuery }) => {
+  return (
+    <Head>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta
+        property="og:url"
+        content={`https://connor-devlin-portfolio.vercel.app${urlQuery}`}
+      />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+    </Head>
+  );
+};
+
+export default DynamicHead;
